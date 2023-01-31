55495253a1953e6c3236cc07a0d95587ef9dc18d
stastk <me@stas.tk>
2023-01-31 10:10:11 +0300

    remap init test

--- app.rb
+++ app.rb
@@ -27,11 +27,24 @@ class Remapper < Sinatra::Base
 
   get '/glyphs_list' do
     # TODO Glyphs from ELAJC and from PHYREXIAN must be placed in to DB
-    %w(q w e r t y)
   end
 
-  post '/remap' do
-    to_be_remapped = params[:t]
+  get '/remap' do
+    @original = params[:t]
+    @to_be_remapped = ""
+    @arr_phie = %w(e b d u i k Q E w h c m n o p C r L t Y l v s y & K O A % N Z H T J a I B U F G z V R $ M P f S W D | g / ~ .)
+    @arr_phie << "\\"
+    @arr_phie << ","
+
+    @arr_wotc = %w(a b d e f g h i j k l m n o p q r s t u v w x y z ð ø č ķ ŋ θ ţ ť ž ǆ ǥ ǧ ǩ ɒ ə ɢ ɣ ɬ ɮ ɴ ʁ θ π φ χ ẅ ’ ^ š : " . /)
+    @arr_wotc << " "
+
+    @original.chars.each do |char|
+      i = @arr_phie.find_index(char)
+      @to_be_remapped += @arr_wotc[i]
+    end
+
+    erb :remapper
   end
 end
 
