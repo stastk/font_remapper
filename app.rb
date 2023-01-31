4215d137df8df3f48b94637fd56e6231659751ba
stastk <me@stas.tk>
2023-02-01 00:26:40 +0300

    remapper as method + websocket

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'sinatra-websocket'
 
+ARR_PHIE = %w(| e b d u i k Q E w h c m n o p C r L t Y l v s y & K O A % N Z H T J a _ I B U F G z V R $ M P f , S W D | g / ~ . \\ ₀ ₁ ₂ ₃ ₄ ₅ ₆ ₇ ₈ ₉ ⁰ ¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹)
+QRR_wotc = %w(^ a b d e f g h i j k l m n o p q r s t u v w x y z ð ø č ķ ŋ θ ţ ť ž ǆ ǥ ǧ ǩ ɒ ə ɢ ɣ ɬ ɮ ɴ ʁ θ π φ χ ẅ ’ ^ š : " . / -0 -1 -2 -3 -4 -5 -6 -7 -8 -9 +0 +1 +2 +3 +4 +5 +6 +7 +8 +9)
+
 class Remapper < Sinatra::Base
   set :server, 'thin'
   set :sockets, []
@@ -11,11 +14,12 @@ class Remapper < Sinatra::Base
     else
       request.websocket do |ws|
         ws.onopen do
-          ws.send("All will be one")
+          #ws.send("All will be one")
           settings.sockets << ws
         end
         ws.onmessage do |msg|
-          EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+          EM.next_tick { settings.sockets.each{|s| s.send(self.remap(msg)) } }
+
         end
         ws.onclose do
           warn("websocket closed")
@@ -29,22 +33,17 @@ class Remapper < Sinatra::Base
     # TODO Glyphs from ELAJC and from PHYREXIAN must be placed in to DB
   end
 
-  get '/remap' do
-    @original = params[:t]
+  def remap(t)
     @to_be_remapped = ""
-    @arr_phie = %w(e b d u i k Q E w h c m n o p C r L t Y l v s y & K O A % N Z H T J a I B U F G z V R $ M P f S W D | g / ~ .)
-    @arr_phie << "\\"
-    @arr_phie << ","
-
-    @arr_wotc = %w(a b d e f g h i j k l m n o p q r s t u v w x y z ð ø č ķ ŋ θ ţ ť ž ǆ ǥ ǧ ǩ ɒ ə ɢ ɣ ɬ ɮ ɴ ʁ θ π φ χ ẅ ’ ^ š : " . /)
-    @arr_wotc << " "
-
-    @original.chars.each do |char|
+    @arr_phie = ARR_PHIE
+    @arr_wotc = QRR_wotc
+    t.chars.each do |char|
       i = @arr_phie.find_index(char)
-      @to_be_remapped += @arr_wotc[i]
+      @to_be_remapped += i.nil? ? char : @arr_wotc[i]
     end
 
-    erb :remapper
+    @to_be_remapped.to_s
+    #erb :remapper
   end
 end
 
